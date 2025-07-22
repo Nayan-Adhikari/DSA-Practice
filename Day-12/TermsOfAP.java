541b3d0ad12f57fae11ea8af87b8fef8b8f88f46
Nayan Adhikari <mail2nayan2004@gmail.com>
2025-07-23 03:47:40 +0530

    Day-12

---
+++ Day-12/TermsOfAP.java
@@ -0,0 +1,18 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int[] termsOfAP(int x){
+		// Write your code here.
+		int []arr = new int[x];
+		int i=1;
+		int count=0;
+		while (count<x){
+			if(((3*i)+2)%4 !=0){
+				arr[count] = ((3*i)+2);
+				count++;
+			}
+			i++;
+		}
+		return arr;
+	}
+}
